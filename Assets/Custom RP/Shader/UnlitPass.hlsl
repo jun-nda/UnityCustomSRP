096290ac72c623a891d074c15a97b9379b64f185
yujd <yujd@mail.jj.cn>
2023-04-03 18:31:44 +0800

    temmm

--- Assets/Custom RP/Shader/UnlitPass.hlsl
+++ Assets/Custom RP/Shader/UnlitPass.hlsl
@@ -3,13 +3,16 @@
 
 #include "../ShaderLibrary/Common.hlsl"
 
+CBUFFER_START(UnityPerMaterial)
+float4 _BaseColor;
+CBUFFER_END
+
 float4 UnlitPassVertex (float3 positionOS : POSITION) : SV_POSITION{
     float3 positionWS = TransformObjectToWorld(positionOS.xyz);
 	return TransformWorldToHClip(positionWS);
 }
 
 float4 UnlitPassFragment () : SV_TARGET{
-    return 0.0;
+    return _BaseColor;
 }
-
 #endif
--- Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -1,8 +1,19 @@
 #ifndef CUSTOM_UNITY_INPUT_INCLUDED
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
+CBUFFER_START(UnityPerDraw)
 float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+float4 unity_LODFade;
+real4 unity_WorldTransformParams; // 暂时不知道是干啥的
+CBUFFER_END
+
 
 float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+float4x4 unity_PreMatrixM;
+float4x4 unity_PreMatrixIM;
+
 
 #endif
